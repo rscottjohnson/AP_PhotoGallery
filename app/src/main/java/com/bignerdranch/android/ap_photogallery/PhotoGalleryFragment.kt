a94a830e5ac2bdcaa8ea3d2196be3c15ed0a69b3
rscottjohnson <sjohnson.wvu@gmail.com>
2020-06-16 16:09:09 -0400

    Networking basics with retrofit

--- app/src/main/java/com/bignerdranch/android/ap_photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/ap_photogallery/PhotoGalleryFragment.kt
@@ -1,16 +1,39 @@
 package com.bignerdranch.android.ap_photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.*
+
+private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment() {
 
     private lateinit var photoRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val flickrLiveData: LiveData<String> = FlickrFetchr().fetchContents()
+        flickrLiveData.observe(
+            this,
+            androidx.lifecycle.Observer { responseString ->
+                Log.d(TAG, "Response received: $responseString")
+            }
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
