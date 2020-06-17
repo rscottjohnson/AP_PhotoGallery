d5e6da74f2887b13628faa3e07c68eaaea3be313
rscottjohnson <sjohnson.wvu@gmail.com>
2020-06-16 21:43:51 -0400

    Networking across configuration changes

--- app/src/main/java/com/bignerdranch/android/ap_photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/ap_photogallery/PhotoGalleryFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import api.FlickrApi
@@ -21,18 +22,13 @@ private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment() {
 
+    private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val flickrLiveData: LiveData<List<GalleryItem>> = FlickrFetchr().fetchPhotos()
-        flickrLiveData.observe(
-            this,
-            androidx.lifecycle.Observer { galleryItems ->
-                Log.d(TAG, "Response received: $galleryItems")
-            }
-        )
+        photoGalleryViewModel = ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
     }
 
     override fun onCreateView(
@@ -46,6 +42,17 @@ class PhotoGalleryFragment : Fragment() {
         photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        photoGalleryViewModel.galleryItemLiveData.observe(
+            viewLifecycleOwner,
+            androidx.lifecycle.Observer { galleryItems ->
+                Log.d(TAG, "Have gallery items from ViewModel $galleryItems")
+                // Eventually, update data backing the recycler view
+            }
+        )
     }
 
     companion object {
