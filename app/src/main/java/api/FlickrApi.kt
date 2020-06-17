a85c0a2a2406f960f7c25dff2a27a70e9fc81c89
rscottjohnson <sjohnson.wvu@gmail.com>
2020-06-17 13:14:54 -0400

    Preparing to Download Bytes from a URL

--- app/src/main/java/api/FlickrApi.kt
+++ app/src/main/java/api/FlickrApi.kt
@@ -1,7 +1,9 @@
 package api
 
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface FlickrApi {
     @GET("services/rest/?method=flickr.interestingness.getList" +
@@ -11,4 +13,7 @@ interface FlickrApi {
             "&extras=url_s"
     )
     fun fetchPhotos(): Call<FlickrResponse>
+
+    @GET
+    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
 }
